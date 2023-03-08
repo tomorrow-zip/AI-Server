4d0f4b4246ed09f8d365441c7e673f1845408563
Park Se Hyeon <qkrtpgus2280@naver.com>
2023-03-08 16:05:54 +0900

    세부 API 구현 완료 및 목적에 맞는 코드 위치로 이동

--- service.py
+++ service.py
@@ -1,95 +1,28 @@
-from mmdet.apis import inference_detector
-from bentoml.io import Image, JSON, NumpyNdarray
+# models
+from models import detection_model, recommend_model, classifier_model
+
+# Runners
+from runner.detector_runnable import DetectorRunnable
+from runner.recommender_runnable import RecommenderRunnable
+
+# utils
+import bentoml
+from bentoml.io import Image, JSON, Multipart
+
 from flask import Flask
 
-from processing import *
-
-from umap import UMAP
-
-import bentoml
 import numpy as np
-import cv2
-import pandas as pd
-import pickle
-import torch
 
-import uuid
-import scipy.spatial.distance as distance
+# processing
+from processing import detect_process, classifier_process, make_bbox_images
 
 
-detection_model = bentoml.pytorch.get("detection_and_segmentation:latest")
-classifier_model = bentoml.keras.get("style-classifier:latest")
-recommend_model = bentoml.picklable_model.get("recommender_furniture:latest")
-
-
-"""
-Custom Runner 정의
-"""
-
-
-class MMDetectionRunnable(bentoml.Runnable):
-    SUPPORTED_RESOURCES = ("nvidia.com/cuda", "cpu")
-    SUPPORTS_CPU_MULTI_THREADING = True
-
-    def __init__(self):
-        self.model = bentoml.pytorch.load_model(detection_model)
-        if torch.cuda.is_available():
-            self.model.cuda()
-        # elif torch.backends.mps.is_available():
-        #     self.model = self.model.to("mps")
-
-        self.classes = self.model.CLASSES
-
-    @bentoml.Runnable.method(batchable=True, batch_dim=0)
-    def detect(self, input_tensor):
-        return inference_detector(self.model, input_tensor)
-
-
-class RecommenderFurnitureRunnable(bentoml.Runnable):
-    SUPPORTED_RESOURCES = "cpu"
-
-    def __init__(self):
-        self.model = bentoml.picklable_model.load_model(recommend_model)
-        self.furniture_style = recommend_model.custom_objects["furniture_style"]
-
-        self.cate_num = {1: 3, 2: 1, 3: 4}  # 침대, 소파, 옷장
-
-    def calculate_similarity(self, vector1, vector2):
-        return distance.euclidean(vector1[0], vector2[0])
-
-    def get_vector(self, values):
-        vectors = self.model.transform(values)
-        return vectors
-
-    @bentoml.Runnable.method(batchable=True, batch_dim=0)
-    def compare_similarity(self, arr, cate=0):
-        # image_arr 를 벡터화시킴
-        vector_obj = self.get_vector([arr])
-        # 유사도를 구함
-        self.furniture_style["similarity"] = self.furniture_style["vector"].apply(
-            lambda x: self.calculate_similarity(x, vector_obj)
-        )
-
-        furniture_sort = self.furniture_style.sort_values(
-            by=["similarity"], ascending=True
-        )
-        if cate == 0:
-            return furniture_sort.head(10)
-        else:
-            return furniture_sort[furniture_sort["cate"] == self.cate_num[cate]].head(
-                10
-            )
-
-
-"""
-Runner 선언
-"""
-detector_runner = bentoml.Runner(MMDetectionRunnable, models=[detection_model])
-recommend_runner = bentoml.Runner(
-    RecommenderFurnitureRunnable, models=[recommend_model]
-)
+detector_runner = bentoml.Runner(DetectorRunnable, models=[detection_model])
+recommend_runner = bentoml.Runner(RecommenderRunnable, models=[recommend_model])
 classifier_runner = classifier_model.to_runner()
 
+
+# service 선언
 svc = bentoml.Service(
     "tomorrow-zip-ai-api",
     runners=[detector_runner, classifier_runner, recommend_runner],
@@ -111,46 +44,40 @@ def detect(input_img):
     return detect_process(result, classes)
 
 
-@svc.api(input=NumpyNdarray(), output=JSON())
-def classifier(input_images):
-    input_img = np.array(input_images, dtype=np.uint8)
+@svc.api(
+    input=Multipart(image=Image(), detected_objects=JSON()),
+    output=JSON(),
+)
+def classifier(image, detected_objects):
+    image = np.array(image, dtype=np.uint8)
 
-    if input_img.shape[0] != 100 and input_img.shape[1] != 100:
-        input_img = cv2.resize(input_img, (100, 100))
+    layers, label_list = make_bbox_images(detected_objects, image)
+    result = classifier_runner.run(layers)
 
-    input_img = input_img[np.newaxis]
-    result_classfied = classifier_runner.run(input_img)
-    return classifier_process(result_classfied)
+    return classifier_process(result, label_list)
 
 
 @svc.api(input=JSON(), output=JSON())
-def recommend(classified_categories):
+def recommend(classified):
     sim = {}
-    for label, styles in classified_categories.items():
+
+    for label, styles in classified.items():
         sim[label] = list(recommend_runner.run(styles).index)
 
     return {"result": sim}
 
 
 @svc.api(input=Image(), output=JSON())
-def unified(input_img):
-    classes = detection_model.custom_objects["classes"]
-    res = detect_process(input_img, classes)
-    img = np.array(input_img, dtype=np.uint8)
-
-    detected_classes = res["detected_class"]
-    detected_objects = res["results"]["objects"]
-
-    layers, label_list = make_bbox_images(detected_objects, img)
-    clas_pred = classifier_process(layers, label_list)
-    recommended_objects = find_similarity(clas_pred)
+def unified(image):
+    res = detect(image)
+    clas_pred = classifier(image, res["results"]["objects"])
+    recommendation = recommend(clas_pred)
 
     return {
-        "state": "success",
-        "detected_class": detected_classes,
-        "detected_object_location": detected_objects,
+        "detected_class": res["detected_class"],
+        "detected_objects": res["results"]["objects"],
         "style": clas_pred,
-        "recom": recommended_objects,
+        "recommendation": recommendation,
     }
 
 
@@ -164,7 +91,15 @@ svc.mount_wsgi_app(app)
 @app.route("/metadata")
 def metadata():
     return {
-        "name": detection_model.tag.name,
-        "version": detection_model.tag.version,
-        "classes": len(detection_model.custom_objects["classes"]),
+        "detection-model-name": detection_model.tag.name,
+        "detection-model-version": detection_model.tag.version,
+        "detection-model-classes": len(detection_model.custom_objects["classes"]),
+        "style-classifier-model-name": classifier_model.tag.name,
+        "style-classifier-version": classifier_model.tag.version,
+        "style-classifier-classes": len(classifier_model.custom_objects["classes"]),
+        "recommender-model-name": recommend_model.tag.name,
+        "recommender-model-version": recommend_model.tag.version,
+        "recommender-model-classes": len(
+            recommend_model.custom_objects["furniture_style"]
+        ),
     }
